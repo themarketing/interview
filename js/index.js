90c9c678c0184fca21c9293b5225ddd85a30bf86
小林 照幸 <koba1014@gmail.com>
2016-02-23 12:57:57 +0900

    css

--- js/index.js
+++ js/index.js
@@ -5,44 +5,58 @@ function initQuestion(dom, obj) {
     });
 }
 function applyPerson2(obj, subobj) {
-    if (obj["@type"] === "Person") {
+    if (subobj["@type"] === "Person") {
         if (typeof subobj["image"] !== "undefined") {
-            obj["image"] = subobj["image"];
+            if (subobj["url"] === obj["author"]["url"]) {
+                obj["author"]["image"] = subobj["image"];
+            }
+            if (subobj["url"] === obj["acceptedAnswer"]["author"]["url"]) {
+                obj["acceptedAnswer"]["author"]["image"] = subobj["image"];
+            }
         }
     }
-    console.log(obj);
-    console.log(subobj);
     return obj;
+}
+function a(obj, aa, fn2) {
+    if (typeof aa !== "undefined") {
+        getContextFromHTTP(aa, function (subdom) {
+            getJSONLDs(subdom).map(function (subobj) {
+                applyPerson2(obj, subobj);
+                fn2(obj);
+                return obj;
+            });
+        });
+    }
+    else {
+        fn2(obj);
+        return obj;
+    }
 }
 function applyQuestion(dom, obj, fn) {
     if (obj["@type"] === "Question") {
-        if (typeof obj["author"]["url"] !== "undefined") {
-            getContextFromHTTP(obj["author"]["url"], function (subdom) {
-                getJSONLDs(subdom).map(function (subobj) {
-                    obj = applyPerson2(obj, subobj);
-                });
-            });
+        if (typeof obj["author"]["image"] === "undefined") {
+            obj["author"]["image"] = "";
         }
-        if (typeof obj["acceptedAnswer"]["author"]["url"] !== "undefined") {
-            getContextFromHTTP(obj["acceptedAnswer"]["author"]["url"], function (subdom) {
-                getJSONLDs(subdom).map(function (subobj) {
-                    obj = applyPerson2(obj, subobj);
-                });
-            });
+        if (typeof obj["acceptedAnswer"]["author"]["image"] === "undefined") {
+            obj["acceptedAnswer"]["author"]["image"] = "";
         }
-        [
-            { selector: ".rpQuestionText", after: obj["text"], fn: changeTXT },
-            { selector: ".rpAnswerText", after: obj["acceptedAnswer"]["text"], fn: changeTXT },
-            { selector: ".rpQuestionPersonName", after: getAuthorName(obj["author"]), fn: changeTXT },
-            { selector: ".rpAnswerPersonName", after: getAuthorName(obj["acceptedAnswer"]["author"]), fn: changeTXT },
-            { selector: ".rpQuestionPersonImage", after: obj["author"]["image"], fn: changeSRC },
-            { selector: ".rpAnswerPersonImage", after: obj["acceptedAnswer"]["author"]["image"], fn: changeSRC }
-        ].forEach(function (a) {
-            return applyDOM(dom, a);
+        a(obj, obj["author"]["url"], function (obj) {
+            a(obj, obj["acceptedAnswer"]["author"]["url"], function (obj) {
+                [
+                    { selector: ".rpQuestionText", after: obj["text"], fn: changeTXT },
+                    { selector: ".rpAnswerText", after: obj["acceptedAnswer"]["text"], fn: changeTXT },
+                    { selector: ".rpQuestionPersonName", after: getAuthorName(obj["author"]), fn: changeTXT },
+                    { selector: ".rpAnswerPersonName", after: getAuthorName(obj["acceptedAnswer"]["author"]), fn: changeTXT },
+                    { selector: ".rpQuestionPersonImage", after: obj["author"]["image"], fn: changeSRC },
+                    { selector: ".rpAnswerPersonImage", after: obj["acceptedAnswer"]["author"]["image"], fn: changeSRC }
+                ].map(function (a) {
+                    return applyDOM(dom, a);
+                });
+                if (typeof obj["author"]["url"] === "undefined") {
+                    fn(dom, obj);
+                }
+            });
         });
-        if (typeof obj["author"]["url"] === "undefined") {
-            fn(dom, obj);
-        }
     }
     return dom;
 }
--- js/index.ts
+++ js/index.ts
@@ -2,49 +2,64 @@
 
 function initQuestion(dom, obj) {
     applyQuestion(dom, obj, (dom, obj) => {
-        //applyPerson(dom, obj);
         addDOM(dom);
     });
 }
-function applyPerson2(obj, subobj): any {
-    if (obj[`@type`] === `Person`) {
-        if (typeof subobj[`image`] !== "undefined") {
-            obj[`image`] = subobj[`image`];
+function applyPerson2(obj, subobj) {
+    if (subobj["@type"] === "Person") {
+        if (typeof subobj["image"] !== "undefined") {
+            if (subobj["url"] === obj["author"]["url"]) {
+                obj[`author`]["image"] = subobj["image"];
+            }
+            if (subobj["url"] === obj["acceptedAnswer"]["author"]["url"]) {
+                obj["acceptedAnswer"]["author"]["image"] = subobj["image"];
+            }
         }
     }
-    console.log(obj);
-    console.log(subobj);
     return obj;
 }
-function applyQuestion(dom: HTMLElement, obj, fn): HTMLElement {
-    if (obj[`@type`] === `Question`) {
-        if (typeof obj[`author`][`url`] !== "undefined") {
-            getContextFromHTTP(obj[`author`][`url`], (subdom) => {
-                getJSONLDs(subdom).map((subobj) => {
-                    obj = applyPerson2(obj, subobj);
-                });
+function a(obj, aa, fn2) {
+    if (typeof aa !== "undefined") {
+        getContextFromHTTP(aa, (subdom) => {
+            getJSONLDs(subdom).map((subobj) => {
+                applyPerson2(obj, subobj);
+                fn2(obj);
+                return obj;
             });
-        }
-        if (typeof obj[`acceptedAnswer`][`author`][`url`] !== "undefined") {
-            getContextFromHTTP(obj[`acceptedAnswer`][`author`][`url`], (subdom) => {
-                getJSONLDs(subdom).map((subobj) => {
-                    obj = applyPerson2(obj, subobj);
-                });
-            });
-        }
-        [
-            { selector: ".rpQuestionText", after: obj[`text`], fn: changeTXT },
-            { selector: ".rpAnswerText", after: obj[`acceptedAnswer`][`text`], fn: changeTXT },
-            { selector: ".rpQuestionPersonName", after: getAuthorName(obj[`author`]), fn: changeTXT },
-            { selector: ".rpAnswerPersonName", after: getAuthorName(obj[`acceptedAnswer`][`author`]), fn: changeTXT },
-            { selector: ".rpQuestionPersonImage", after: obj[`author`][`image`], fn: changeSRC },
-            { selector: ".rpAnswerPersonImage", after: obj[`acceptedAnswer`][`author`][`image`], fn: changeSRC }
-        ].forEach((a) => {
-            return applyDOM(dom, a);
         });
-        if (typeof obj[`author`][`url`] === "undefined") {
-            fn(dom, obj);
+    } else {
+        fn2(obj);
+        return obj;
+    }
+}
+function applyQuestion(dom: HTMLElement, obj, fn): HTMLElement {
+    if (obj["@type"] === "Question") {
+        if (typeof obj["author"]["image"] === "undefined") {
+            obj["author"]["image"] = "";
         }
+        if (typeof obj["acceptedAnswer"]["author"]["image"] === "undefined") {
+            obj["acceptedAnswer"]["author"]["image"] = "";
+        }
+        a(obj, obj["author"]["url"], (obj) => {
+            a(obj, obj["acceptedAnswer"]["author"]["url"], (obj) => {
+                [
+                    { selector: ".rpQuestionText", after: obj["text"], fn: changeTXT },
+                    { selector: ".rpAnswerText", after: obj["acceptedAnswer"]["text"], fn: changeTXT },
+                    { selector: ".rpQuestionPersonName", after: getAuthorName(obj["author"]), fn: changeTXT },
+                    { selector: ".rpAnswerPersonName", after: getAuthorName(obj["acceptedAnswer"]["author"]), fn: changeTXT },
+                    { selector: ".rpQuestionPersonImage", after: obj["author"]["image"], fn: changeSRC },
+                    { selector: ".rpAnswerPersonImage", after: obj["acceptedAnswer"]["author"]["image"], fn: changeSRC }
+                ].map((a) => {
+                    return applyDOM(dom, a);
+                });
+                if (typeof obj[`author`][`url`] === "undefined") {
+                    fn(dom, obj);
+                }
+
+            });
+        });
+
+
     }
     return dom;
 }
