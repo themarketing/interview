4358235e609b8bfbc3e14cdb4480c28e78d6811c
小林 照幸 <koba1014@gmail.com>
2016-02-24 13:50:03 +0900

    js

--- js/index.ts
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-initModule("//interview.ninkigumi.com/templ.html",
-    "//interview.ninkigumi.com/organization/family1st/sitemap.txt", initQuestion);
-//initModule("/marketing/interview/templ.html",
-//    "/marketing/interview/organization/family1st/sitemap.txt", initQuestion);
--- js/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-initModuleFromSelf("https://profile.ninkigumi.com/templ.html", function (dom, obj) {
-    applyPerson(dom, obj);
-    addDOM(dom);
-});
